1da94dcedfdddb5ec0bd7d7579efc968f87e636c
Jesus R Lopez Martin <jesus.reikel@gmail.com>
2022-07-03 02:23:31 -0400

    Mapeo de memoria

--- src/main/main.asm
+++ src/main/main.asm
@@ -2,6 +2,18 @@ main:
     
 
     call frame
+    mov bh, 100; fila
+    mov bl, 100; columna
+    call drawRedball
     ; call darkhole
 
 jmp $
+
+    video:
+        push eax
+        mov eax, 0fd000000h
+        mov edi, eax
+        pop eax
+    ret
+
+    res dw 1
--- src/map/frame.asm
+++ src/map/frame.asm
@@ -1,29 +1,55 @@
 frame: 
-    ; mov cx, 512
-    xor di, di
+    mov cx, 512
     mov al, 0001b
     mov ah, 0001b
-    ; @@:
-    ;     mov [es:di], ax
-    ;     mov [es:di+1024], ax
-    ;     mov [es:di+2048], ax
-    ;     mov [es:di+3076], ax
-    ;     mov [es:di+4096], ax
-    ;     mov [es:di+5120], ax
-    ;     mov [es:di+6144], ax
-    ;     mov [es:di+7168], ax
-    ;     mov [es:di+8192], ax
-    ;     mov [es:di+9216], ax
-    ;     mov [es:di+10240], ax
-    ;     add di, 2
-    ;     loop @b 
-
-    xor di, di
-    mov cx, 768
-    mov edx, 0a000h
+    
     @@:
-        mov [edx], ax
-        add edx, 1024
+        mov [edi], ax
+        mov [edi+1024], ax
+        mov [edi+1024*2], ax
+        mov [edi+1024*3], ax
+        mov [edi+1024*4], ax
+        mov [edi+1024*5], ax
+        mov [edi+1024*6], ax
+        mov [edi+1024*7], ax
+        mov [edi+1024*8], ax
+        mov [edi+1024*9], ax
+        mov [edi+1024*10], ax
+        
+        add edi, 2
+        loop @b 
+
+    @@:
+        mov [edi+1024*768], ax
+        mov [edi+1024*767], ax
+        mov [edi+1024*766], ax
+        mov [edi+1024*765], ax
+        mov [edi+1024*764], ax
+        mov [edi+1024*763], ax
+        mov [edi+1024*762], ax
+        mov [edi+1024*761], ax
+        mov [edi+1024*760], ax
+        mov [edi+1024*759], ax
+        mov [edi+1024*758], ax
+
+        add edi, 2
+        loop @b 
+
+    call video
+    mov cx, 768
+    @@:
+        mov [edi], ax
+        mov [edi+2], ax
+        mov [edi+4], ax
+        mov [edi+6], ax
+        mov [edi+8], ax
+        mov [edi+1022], ax
+        mov [edi+1020], ax
+        mov [edi+1018], ax
+        mov [edi+1016], ax
+        mov [edi+1014], ax
+        add edi, 1024
         loop @b
+
 
 ret
